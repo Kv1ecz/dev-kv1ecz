11c4cad2450c41f3be5c86483c5b4e218aea901c
Kv1ecz <lfkvcz@gmail.com>
2022-06-28 16:04:38 -0300

    Editing the app branding (title and icon).

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header(){
     return(
         <header className="flex p-5 w-full bg-gray-700 border-b border-gray-600 items-center absolute">
-            <div className="group">
+            <div className="flex group">
                 <span className="text-[1.5rem] cursor-pointer text-sky-500 font-bold group-hover:text-yellow-200 transition-colors">{"<Dev/> "}</span>
                 <span className="text-[1.5rem] cursor-pointer text-yellow-200 group-hover:text-sky-500 transition-colors">Kv1ecz</span>
             </div>
